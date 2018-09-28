4986c5eb976c133eba6ebde0c672a45bfa8b6daa
changping <2623116435@qq.com>
2018-09-28 11:04:05 +0800

    xiaoyao is born today first

--- xiaoyao/settings.py
+++ xiaoyao/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/workspace/nginx_static_file/static/'
+STATIC_ROOT = '/opt/nginx_static_file/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
