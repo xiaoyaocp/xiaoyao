2bb28fa555ca9e9ac4c1abd2175fa7838df153fe
changping <2623116435@qq.com>
2018-10-19 10:11:07 +0800

    develop branch

--- ts.sh
+++ ts.sh
@@ -5,6 +5,5 @@ echo $DEPLOY_ROOT
 IMAGE_NAME="xiaoyao:web"
 echo $IMAGE_NAME
 
-docker build -f rain_django.dockerfile -t ${IMAGE_NAME} ${PROJECT_ROOT}
 cd ${DEPLOY_ROOT}
 docker build -f Dockerfile -t ${IMAGE_NAME} ${DEPLOY_ROOT}
